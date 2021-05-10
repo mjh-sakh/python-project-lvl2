3b5dd22b0b64ba3aaf8d5e2b864f191f11db406b
MjH.Sakh <48555070+mjh-sakh@users.noreply.github.com>
2021-05-10 19:57:07 +1100

    new first base logic

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,5 +1,61 @@
 import json
 
 
-def generate_diff(file1, file2):
+def generate_diff(file1: str, file2: str) -> str:
     return "Yep, it's string - going with TDD, bro :)"
+
+
+def read_json_from_path_to_dict(file_path: str) -> dict:
+    with open(file_path, 'r') as file:
+        json_dict = json.load(file)
+    return json_dict
+
+
+def get_comparison_for_two_dicts(dict1: dict, dict2: dict):
+    keys1 = dict1.keys()
+    keys2 = dict2.keys()
+    all_keys = keys1 | keys2
+    comparisons = Comparisons()
+    flag = ""
+    value = None
+    for key in sorted(list(all_keys)):
+        if key not in dict1:  # key only in dict2
+            flag = "+"
+            value = dict2[key]
+            comparisons.add_item(flag, key, value)
+        elif key not in dict2:  # key only in dict1
+            flag = "-"
+            value = dict1[key]
+            comparisons.add_item(flag, key, value)
+        else:
+            value1 = dict1[key]
+            value2 = dict2[key]
+            if value1 == value2:
+                flag = " "
+                comparisons.add_item(flag, key, value1)
+            else:
+                flag = "-"
+                comparisons.add_item(flag, key, value1)
+                flag = "+"
+                comparisons.add_item(flag, key, value2)
+    return comparisons
+
+
+class Comparisons(object):
+    def __init__(self):
+        self.comparison_list = []
+
+    def add_item(self, flag, key, value):
+        value = self.convert_value(value)
+        self.comparison_list.append((flag, key, value))
+
+    def convert_value(self, value):
+        if type(value) == bool:
+            return "true" if value else "false"
+        return str(value)
+
+    def __repr__(self):
+        return str(self.comparison_list)
+
+    def __eq__(self, other):
+        return self.comparison_list == other
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -2,13 +2,46 @@ import pytest
 from gendiff import generate_diff
 
 
-@pytest.fixture
-def test_json_files_1():
-    file1_path = "file1.json"
-    file2_path = "file2.json"
-    return file1_path, file2_path
+@pytest.mark.parametrize("file1, file2", [
+    ("file1.json", "file2.json"),
+])
+def test_return_type_is_string(file1, file2):
+    assert type(generate_diff.generate_diff(file1, file2)) == str
 
 
-def test_return_type_is_string(test_json_files_1):
-    file1_path, file2_path = test_json_files_1
-    assert type(generate_diff.generate_diff(file1_path, file2_path)) == str
+# def test_output_string_format():
+#     empty_json = "empty.json"
+#     simple_json = "simple.json"
+#     resulting_string = generate_diff(empty_json, simple_json)
+#
+#
+# def test_same_file():
+#     file1 = file2 = "file1.json"
+#     file_content = json.load(file1)
+#     assert generate_diff
+
+@pytest.mark.parametrize("file_path, expected_result", [
+    ("tests/empty.json", {}),
+    ("tests/simple.json", {"test": 1}),
+])
+def test_read_json_from_path_to_dict(file_path, expected_result):
+    assert generate_diff.read_json_from_path_to_dict(file_path) == expected_result
+
+
+@pytest.mark.parametrize("file1, file2, expected_result", [
+    ("tests/empty.json", "tests/simple.json", [("+", "test", "1")]),
+    ("tests/simple.json", "tests/empty.json", [("-", "test", "1")]),
+    ("tests/simple.json", "tests/simple.json", [(" ", "test", "1")]),
+])
+def test_comparison_(file1, file2, expected_result):
+    dict1 = generate_diff.read_json_from_path_to_dict(file1)
+    dict2 = generate_diff.read_json_from_path_to_dict(file2)
+    assert generate_diff.get_comparison_for_two_dicts(dict1, dict2) == expected_result
+
+
+def test_Comparisons_class_equals():
+    comp1 = generate_diff.Comparisons()
+    comp1.add_item(" ", "key", "value")
+    assert comp1 == comp1
+    assert comp1 == [(" ", "key", "value")]
+    assert not comp1 == []
