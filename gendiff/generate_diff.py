f78cb6b82d192ae12607702bade4ab78ad09ab96
MjH.Sakh <48555070+mjh-sakh@users.noreply.github.com>
2021-05-10 20:58:20 +1100

    new first running code for diff

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -2,13 +2,34 @@ import json
 
 
 def generate_diff(file1: str, file2: str) -> str:
-    return "Yep, it's string - going with TDD, bro :)"
+    dict1 = read_json_from_path_to_dict(file1)
+    dict2 = read_json_from_path_to_dict(file2)
+    comparisons = get_comparison_for_two_dicts(dict1, dict2)
+    comparisons_string = generate_comparison_output_string(comparisons)
+    return comparisons_string
 
 
 def read_json_from_path_to_dict(file_path: str) -> dict:
     with open(file_path, 'r') as file:
         json_dict = json.load(file)
     return json_dict
+
+
+class Comparisons(list):
+    def __init__(self):
+        super().__init__()
+
+    def add_item(self, flag, key, value):
+        value = self.convert_value_to_string(value)
+        self.append((flag, key, value))
+
+    def convert_value_to_string(self, value):
+        if type(value) == bool:
+            return "true" if value else "false"
+        return str(value)
+
+    # def __getitem__(self, item):
+    #     return dict(zip(["flag", "key", "value"], super().__getitem__(item)))
 
 
 def get_comparison_for_two_dicts(dict1: dict, dict2: dict):
@@ -41,21 +62,12 @@ def get_comparison_for_two_dicts(dict1: dict, dict2: dict):
     return comparisons
 
 
-class Comparisons(object):
-    def __init__(self):
-        self.comparison_list = []
-
-    def add_item(self, flag, key, value):
-        value = self.convert_value(value)
-        self.comparison_list.append((flag, key, value))
-
-    def convert_value(self, value):
-        if type(value) == bool:
-            return "true" if value else "false"
-        return str(value)
-
-    def __repr__(self):
-        return str(self.comparison_list)
-
-    def __eq__(self, other):
-        return self.comparison_list == other
+def generate_comparison_output_string(comparisons) -> str:
+    result_string = "{"
+    flag_to_add_trailing_comma = False
+    for comparison in comparisons:
+        result_string += ",\n" if flag_to_add_trailing_comma else "\n"
+        flag, key, value = comparison
+        result_string += f"  {flag} {key}: {value}"
+    result_string += "\n}"
+    return result_string
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -3,22 +3,21 @@ from gendiff import generate_diff
 
 
 @pytest.mark.parametrize("file1, file2", [
-    ("file1.json", "file2.json"),
+    (r"tests\file1.json", r"tests\file2.json"),
 ])
-def test_return_type_is_string(file1, file2):
+def test_generate_diff_return_type_is_string(file1, file2):
     assert type(generate_diff.generate_diff(file1, file2)) == str
 
 
-# def test_output_string_format():
-#     empty_json = "empty.json"
-#     simple_json = "simple.json"
-#     resulting_string = generate_diff(empty_json, simple_json)
-#
-#
-# def test_same_file():
-#     file1 = file2 = "file1.json"
-#     file_content = json.load(file1)
-#     assert generate_diff
+@pytest.mark.parametrize("file1, file2, expected_result", [
+    ("tests/empty.json", "tests/simple.json", "{\n  + test: 1\n}"),
+    ("tests/simple.json", "tests/empty.json", "{\n  - test: 1\n}"),
+    ("tests/simple.json", "tests/simple.json", "{\n    test: 1\n}"),
+    ("tests/file1.json", "tests/file2.json", "{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}")
+])
+def test_generate_diff(file1, file2, expected_result):
+    assert generate_diff.generate_diff(file1, file2) == expected_result
+
 
 @pytest.mark.parametrize("file_path, expected_result", [
     ("tests/empty.json", {}),
@@ -39,9 +38,45 @@ def test_get_comparison_for_two_dicts(file1, file2, expected_result):
     assert generate_diff.get_comparison_for_two_dicts(dict1, dict2) == expected_result
 
 
+@pytest.fixture
+def example_comparisons():
+    comparisons = generate_diff.Comparisons()
+    comparisons.add_item(" ", "test_str", "value")
+    comparisons.add_item("+", "test_int", 1)
+    comparisons.add_item("+", "test_float", 3.14)
+    comparisons.add_item("+", "test_bool", True)
+    comparisons.add_item("+", "test_bool", False)
+    return comparisons
+
+
+def test_Comparisons_class_content_to_be_all_strings(example_comparisons):
+    for items in example_comparisons:
+        assert all(map(lambda x: type(x) == str, items))
+
+
+def test_Comparisons_class_proper_bool_represenation():
+    comparisons = generate_diff.Comparisons()
+    comparisons.add_item(" ", "True", True)
+    comparisons.add_item(" ", "False", False)
+    assert comparisons[0][2] == "true"
+    assert comparisons[1][2] == "false"
+
+
 def test_Comparisons_class_equals():
-    comp1 = generate_diff.Comparisons()
-    comp1.add_item(" ", "key", "value")
-    assert comp1 == comp1
-    assert comp1 == [(" ", "key", "value")]
-    assert not comp1 == []
+    comparisons = generate_diff.Comparisons()
+    item = (" ", "key", "value")
+    comparisons.add_item(*item)
+    assert comparisons == comparisons
+    assert comparisons == [item]
+    assert not comparisons == []
+
+
+@pytest.mark.parametrize("comparisons_item, expected_result", [
+    (("+", "test", "1"), "{\n  + test: 1\n}"),
+    (("-", "test", "1"), "{\n  - test: 1\n}"),
+    ((" ", "test", "1"), "{\n    test: 1\n}"),
+])
+def test_generate_comparison_output_string_format(comparisons_item, expected_result):
+    comparisons = generate_diff.Comparisons()
+    comparisons.add_item(*comparisons_item)
+    assert generate_diff.generate_comparison_output_string(comparisons) == expected_result
