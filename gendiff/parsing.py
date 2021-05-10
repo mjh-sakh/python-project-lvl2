974bc2f35a21702baa4221372366af4a52750600
MjH.Sakh <48555070+mjh-sakh@users.noreply.github.com>
2021-05-10 22:35:02 +1100

    add file type detection

--- gendiff/parsing.py
+++ gendiff/parsing.py
@@ -1,4 +1,16 @@
 import json
+import os
+
+
+def get_proper_reader_for_file(file_path: str):
+    assert os.path.isfile(file_path), f"{file_path} is not a file."
+    file_name = os.path.split(file_path)[-1]
+    extension = file_name.split('.')[-1]
+    if extension == "json":
+        return read_json_from_path_to_dict
+    elif extension in ("yml", "yaml"):
+        return read_yaml_from_path_to_dict
+    assert True, f"Unsupported file type {extension}"
 
 
 def read_json_from_path_to_dict(file_path: str) -> dict:
