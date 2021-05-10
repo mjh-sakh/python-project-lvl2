7e4222942c456235e3f0595ac5447235ea0daa74
MjH.Sakh <48555070+mjh-sakh@users.noreply.github.com>
2021-05-10 15:26:26 +1100

    lint

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -7,6 +7,7 @@ parser.add_argument("first_file", help="First file to compare.")
 parser.add_argument("second_file", help="Second file to compare.")
 parser.add_argument("-f", "--format", help="Set format of output.")
 
+
 def main():
     args = parser.parse_args()
     print(args)
