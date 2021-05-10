38825a8009ede0df434dd8923e802a2a6d0a26a9
MjH.Sakh <48555070+mjh-sakh@users.noreply.github.com>
2021-05-10 16:42:28 +1100

    add placeholder for gen diff logic

---
+++ gendiff/generate_diff.py
@@ -0,0 +1,5 @@
+import json
+
+
+def generate_diff(file1, file2):
+    assert False, "Not implemented"
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+from gendiff.generate_diff import generate_diff
 
 parser = argparse.ArgumentParser(description="Generate diff.")
 parser.add_argument("first_file", help="First file to compare.")
@@ -11,6 +12,7 @@ parser.add_argument("-f", "--format", help="Set format of output.")
 def main():
     args = parser.parse_args()
     print(args)
+    generate_diff(args.first_file, args.second_file)
 
 
 if __name__ == "__main__":
