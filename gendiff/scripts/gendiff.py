ae493b2b3f7398238d0e26b4e529ce56523c1e6d
MjH.Sakh <48555070+mjh-sakh@users.noreply.github.com>
2021-05-19 21:22:45 +1100

    refactor arg parser into main

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+
 from gendiff.core import generate_diff
 
 DESCRIPTION = r"""
@@ -20,19 +21,20 @@ DESCRIPTION = r"""
     }
 """  # noqa: W291, W293
 
-parser = argparse.ArgumentParser(description="Compares two json objects and shows differences between them.",  # noqa: E501
-                                 usage=DESCRIPTION,
-                                 formatter_class=argparse.RawTextHelpFormatter)
-parser.add_argument("first_file", help="First file to compare.")
-parser.add_argument("second_file", help="Second file to compare.")
-parser.add_argument("-f", "--format", choices=['stylish', 'plain', 'json'],
-                    help="Set format of output. 'stylish' is default.")  # noqa: E501
-
 
 def main():
     """
     Parse gendiff args and print comparison results.
     """
+    parser = argparse.ArgumentParser(description="Compares two json objects and shows differences between them.", # noqa: E501
+                                     usage=DESCRIPTION,
+                                     formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument("first_file", help="First file to compare.")
+    parser.add_argument("second_file", help="Second file to compare.")
+    parser.add_argument("-f", "--format", choices=['stylish', 'plain', 'json'],
+                        help="Set format of output. 'stylish' is default.")  # noqa: E501
+
+
     args = parser.parse_args()
     print(generate_diff(args.first_file,
                         args.second_file,
