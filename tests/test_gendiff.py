30ff0f4b7da1418bd8c86e83b2df2a3fe63551a1
MjH.Sakh <48555070+mjh-sakh@users.noreply.github.com>
2021-05-28 00:03:33 +1100

    refactor tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -6,9 +6,15 @@ import gendiff.formatters.formatter_stylish
 import gendiff.utilities
 from gendiff import generate_diff
 from gendiff import parsing
+from gendiff.utilities import read_text
 
 TEST_FOLDER = "tests"
 FIXTURES_FOLDER = "fixtures"
+
+
+def locate(file_name):
+    file_path = os.path.join(TEST_FOLDER, FIXTURES_FOLDER, file_name)
+    return file_path
 
 
 class TestClassBlackBoxTests:
@@ -16,9 +22,7 @@ class TestClassBlackBoxTests:
         ("file_flat_1.json", "file_flat_2.json"),
     ])
     def test_generate_diff_return_type_is_string(self, file1, file2):
-        file1 = os.path.join(TEST_FOLDER, FIXTURES_FOLDER, file1)
-        file2 = os.path.join(TEST_FOLDER, FIXTURES_FOLDER, file2)
-        assert isinstance(generate_diff(file1, file2), str)
+        assert isinstance(generate_diff(locate(file1), locate(file2)), str)
 
     @pytest.mark.parametrize("file1, file2, expected_result", [
         ("empty.json", "simple.json", "{\n  + test: 1\n}"),
@@ -30,71 +34,60 @@ class TestClassBlackBoxTests:
          "{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}")
     ])
     def test_generate_diff(self, file1, file2, expected_result):
-        file1 = os.path.join(TEST_FOLDER, FIXTURES_FOLDER, file1)
-        file2 = os.path.join(TEST_FOLDER, FIXTURES_FOLDER, file2)
-        assert generate_diff(file1, file2) == expected_result
+        assert generate_diff(locate(file1), locate(file2)) == expected_result
 
     @pytest.mark.parametrize("file1, file2, file_with_expected_result", [
         ("file_nested_1.json", "file_nested_2.json", "output_nested_stylish.txt"),
         ("file_nested_1.yml", "file_nested_2.yaml", "output_nested_stylish.txt")
     ])
     def test_generate_diff_with_recursion(self, file1, file2, file_with_expected_result):
-        file1 = os.path.join(TEST_FOLDER, FIXTURES_FOLDER, file1)
-        file2 = os.path.join(TEST_FOLDER, FIXTURES_FOLDER, file2)
-        file_with_expected_result = os.path.join(TEST_FOLDER, FIXTURES_FOLDER, file_with_expected_result)
-        with open(file_with_expected_result, 'r') as file:
-            expected_result = file.read()
-        assert generate_diff(file1, file2) == expected_result
+        expected_result = read_text(locate(file_with_expected_result))
+        assert generate_diff(locate(file1), locate(file2)) == expected_result
 
     @pytest.mark.parametrize("file1, file2", [
         ("file_flat_1.json", "file_flat_2.json"),
     ])
     def test_generate_diff_with_none_formatter(self, file1, file2):
-        file1 = os.path.join(TEST_FOLDER, FIXTURES_FOLDER, file1)
-        file2 = os.path.join(TEST_FOLDER, FIXTURES_FOLDER, file2)
-        assert isinstance(generate_diff(file1, file2, formatter=None), str)
+        assert isinstance(generate_diff(locate(file1), locate(file2), formatter=None), str)
 
     @pytest.mark.parametrize("file1, file2, file_with_expected_result", [
         ("file_nested_1.json", "file_nested_2.json", "output_nested_plain.txt"),
         ("file_nested_1.yml", "file_nested_2.yaml", "output_nested_plain.txt")
     ])
     def test_generate_diff_with_plain_formatter(self, file1, file2, file_with_expected_result):
-        file1 = os.path.join(TEST_FOLDER, FIXTURES_FOLDER, file1)
-        file2 = os.path.join(TEST_FOLDER, FIXTURES_FOLDER, file2)
-        file_with_expected_result = os.path.join(TEST_FOLDER, FIXTURES_FOLDER, file_with_expected_result)
-        with open(file_with_expected_result, 'r') as file:
-            expected_result = file.read()
-        assert generate_diff(file1, file2, formatter='plain') == expected_result
+        expected_result = read_text(locate(file_with_expected_result))
+        assert generate_diff(locate(file1), locate(file2), formatter='plain') == expected_result
 
     @pytest.mark.parametrize("file1, file2, file_with_expected_result", [
         ("file_nested_1.json", "file_nested_2.yaml", "output_nested_json.txt")
     ])
     def test_generate_diff_with_json_formatter(self, file1, file2, file_with_expected_result):
-        file1 = os.path.join(TEST_FOLDER, FIXTURES_FOLDER, file1)
-        file2 = os.path.join(TEST_FOLDER, FIXTURES_FOLDER, file2)
-        file_with_expected_result = os.path.join(TEST_FOLDER, FIXTURES_FOLDER, file_with_expected_result)
-        with open(file_with_expected_result, 'r') as file:
-            expected_result = file.read()
-        assert generate_diff(file1, file2, formatter='json') == expected_result
+        expected_result = read_text(locate(file_with_expected_result))
+        assert generate_diff(locate(file1), locate(file2), formatter='json') == expected_result
 
 
 class TestClassWhiteBoxTests:
-    @pytest.mark.parametrize("file_path, expected_result", [
+    @pytest.mark.parametrize("file, expected_result", [
+        ("simple.json", '{\n  "test": 1\n}')
+    ])
+    def test_read_text(self, file, expected_result):
+        assert read_text(locate(file)) == expected_result
+
+    @pytest.mark.parametrize("file, expected_result", [
         ("empty.json", {}),
         ("simple.json", {"test": 1}),
     ])
-    def test_read_json_from_path_to_dict(self, file_path, expected_result):
-        file_path = os.path.join(TEST_FOLDER, FIXTURES_FOLDER, file_path)
-        assert parsing.read_json_to_dict(file_path) == expected_result
+    def test_read_json_from_path_to_dict(self, file, expected_result):
+        assert parsing.read_json_to_dict(locate(file)) == expected_result
 
     def test_formatter_proper_bool_representation(self):
         assert gendiff.formatters.formatter_stylish.convert_value_to_string(True) == "true"
         assert gendiff.formatters.formatter_stylish.convert_value_to_string(False) == "false"
 
-    @pytest.mark.parametrize("file_path, expected_result", [
+    @pytest.mark.parametrize("file, expected_result", [
         ("test.json", parsing.read_json_to_dict),
         ("test.yml", parsing.read_yaml_to_dict),
         ("test.yaml", parsing.read_yaml_to_dict)
     ])
-    def test_get_proper_reader_for_file(self, file_path, expected_result):
-        assert parsing.get_proper_read_to_dict(file_path) is expected_result
+    def test_get_proper_reader_for_file(self, file, expected_result):
+        assert parsing.get_proper_read_to_dict(locate(file)) is expected_result
