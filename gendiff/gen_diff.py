952079a1ed433e1cbd096200acf50b5f7b82418a
MjH.Sakh <48555070+mjh-sakh@users.noreply.github.com>
2021-05-28 07:52:14 +1100

    refactor work with text

--- gendiff/gen_diff.py
+++ gendiff/gen_diff.py
@@ -3,8 +3,7 @@ from typing import Optional
 
 from gendiff.comparisons_builder import get_comparisons
 from gendiff.formatters.formatter import get_formatter
-# from gendiff.parsing import get_proper_read_to_dict_for_file
-from gendiff.parsing import get_proper_read_to_dict_for_text
+from gendiff.parsing import get_proper_read_to_dict
 from gendiff.utilities import read_text
 
 
@@ -25,16 +24,10 @@ def generate_diff(
     assert os.path.isfile(file2), f"{file2} is not a file."
     formatter = 'stylish' if formatter is None else formatter
 
-    # read_to_dict = get_proper_read_to_dict_for_file(file1)
-    # dict1 = read_to_dict(file1)
-    file1_text = read_text(file1)
-    read_to_dict = get_proper_read_to_dict_for_text(file1_text)
-    dict1 = read_to_dict(file1_text)
-    # read_to_dict = get_proper_read_to_dict_for_file(file2)
-    # dict2 = read_to_dict(file2)
-    file2_text = read_text(file2)
-    read_to_dict = get_proper_read_to_dict_for_text(file2_text)
-    dict2 = read_to_dict(file2_text)
+    read_to_dict = get_proper_read_to_dict(file1)
+    dict1 = read_to_dict(read_text(file1))
+    read_to_dict = get_proper_read_to_dict(file2)
+    dict2 = read_to_dict(read_text(file2))
     comparisons = get_comparisons(dict1, dict2)
     generate_comparison_output_string = get_formatter(formatter)
     comparisons_string = generate_comparison_output_string(comparisons)
--- gendiff/parsing.py
+++ gendiff/parsing.py
@@ -1,6 +1,6 @@
 import json
 import os
-from typing import Dict, Callable
+from typing import Callable
 
 import yaml
 
@@ -14,40 +14,7 @@ def get_proper_read_to_dict(file_path: str) -> Callable:
     """
     extension = os.path.splitext(file_path)[-1]
     if extension == ".json":
-        return read_json_to_dict
-    elif extension in (".yml", ".yaml"):
-        return read_yaml_to_dict
-    raise NotImplementedError(f"Unsupported file type {extension}")
-
-
-def get_proper_read_to_dict_for_text(text: str) -> Callable:
-    assert len(text) > 0, "Empty string provided."
-    if text[0] == '{':
         return json.loads
-    if text[0] == '-':
+    elif extension in (".yml", ".yaml"):
         return yaml.safe_load
-    raise NotImplementedError(f"Cannot identify data type starting with '{text[0]}'.")  # noqa: E501
-
-
-def read_json_to_dict(file_path: str) -> Dict:
-    """
-    Read json file to Python dictionary.
-
-    :param file_path: str.
-    :return: dict.
-    """
-    with open(file_path, 'r') as file:
-        python_dict = json.load(file)
-    return python_dict
-
-
-def read_yaml_to_dict(file_path: str) -> Dict:
-    """
-    Read yaml file to Python dictionary.
-
-    :param file_path: str.
-    :return: dict.
-    """
-    with open(file_path, 'r') as file:
-        python_dict = yaml.safe_load(file)
-    return python_dict
+    raise NotImplementedError(f"Unsupported file type {extension}")
